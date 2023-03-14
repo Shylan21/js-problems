768d4de7450c96404c6f234f4a188256cccc1144
Isabelle <isabel.tartarelli@live.it>
2023-03-14 15:20:01 +0000

    First -easy- exercise

---
+++ src/easy/digitCount/solution.js
@@ -0,0 +1,10 @@
+/**
+ * Define a function with the 'n' parameter
+ * Convert number to string and get length
+ * Show results
+ */
+
+function numsDigits(n) {
+  return n.toString().length;
+}
+console.log(numsDigits(318));
