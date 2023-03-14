a673c03a4688715ce57d04d18f518586d7c1d146
Isabelle <isabel.tartarelli@live.it>
2023-03-14 17:39:19 +0000

    Factorial exercise completed

--- src/easy/factorial/plain.md
+++ src/easy/factorial/plain.md
@@ -1,1 +1,6 @@
 # Plain English Solution
+
+Declare a 'number'
+Create function
+Loop through every number multiplying them
+Get result
---
+++ src/easy/factorial/solution.js
@@ -0,0 +1,8 @@
+function factorial(n) {
+  let number = 1;
+  for (let i = 1; i <= n; i++) {
+    number *= i;
+  }
+  return number;
+}
+console.log(factorial(5));
